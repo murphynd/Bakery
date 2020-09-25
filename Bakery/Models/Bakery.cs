8c725f6f3b2c99b0cd22b5f3c424414151e46572
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-25 12:14:34 -0400

    First test for BreadSale failed in a good way

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -2,49 +2,62 @@ using System;
 
 namespace Bakery.Models
 {
-  public class Words
+  public class Bread
   {
-    public static int scrabbleSolver(char letter)
+    public static int BreadSale(int input)
     {
-      switch (Char.ToLower(letter))
-      {
-        case 'd':
-        case 'g':
-          return 2;
-        case 'b':
-        case 'c':
-        case 'm':
-        case 'p':
-          return 3;
-        case 'y':
-        case 'w':
-        case 'v':
-        case 'h':
-        case 'f':
-          return 4;
-        case 'k':
-          return 5;
-        case 'j':
-        case 'x':
-          return 8;
-        case 'z':
-        case 'q':
-          return 10;
-        case ' ':
-          return 0;
-        default:
-          return 1;
-      }
+      return 5;
     }
-    public static int getWordScore(string word)
+  }
+  public class Pastery
+  {
+    public static int PasterySale(int input)
     {
-      int TotalScore = 0;
-      foreach (char letter in word)
-      {
-        TotalScore += Words.scrabbleSolver(letter);
-      }
-      return TotalScore;
+      return 6;
     }
-
   }
 }
+//   public static int scrabbleSolver(char letter)
+//   {
+//     switch (Char.ToLower(letter))
+//     {
+//       case 'd':
+//       case 'g':
+//         return 2;
+//       case 'b':
+//       case 'c':
+//       case 'm':
+//       case 'p':
+//         return 3;
+//       case 'y':
+//       case 'w':
+//       case 'v':
+//       case 'h':
+//       case 'f':
+//         return 4;
+//       case 'k':
+//         return 5;
+//       case 'j':
+//       case 'x':
+//         return 8;
+//       case 'z':
+//       case 'q':
+//         return 10;
+//       case ' ':
+//         return 0;
+//       default:
+//         return 1;
+//     }
+//   }
+//   public static int getWordScore(string word)
+//   {
+//     int TotalScore = 0;
+//     foreach (char letter in word)
+//     {
+//       TotalScore += Words.scrabbleSolver(letter);
+//     }
+//     return TotalScore;
+//   }
+
+// }
+
--- README.md
+++ README.md
@@ -1,16 +1,68 @@
-Pierre's Bakery
-Create a C# console application for a bakery that includes the following functionality:
+# 🧑🏼‍🍳 Pierre's Bakery 🥖 🥐 🍰
 
-There should be two classes: one for Bread and one for Pastry.
+#### A C# console application for a bakery
+
+##### Sept 25, 2020
+
+#### By _**Natalie Murphy**_
+
+## Description
 
 When the user runs the application, they should receive a prompt with a welcome message along with the cost for both Bread and Pastry.
-
 A user should be able to specify how many loaves of Bread and how many Pastrys they'd like.
-
 The application will return the total cost of the order.
 
-Pierre offers the following deals:
+two classes: one for Bread and one for Pastry.
 
-Bread: Buy 2, get 1 free. A single loaf costs $5.
-Pastry: Buy 1 for \$2 or 3 for $5.
-All functionality for the models should be tested.
+## Program starts
+
+- welcome to the Bakery!
+- buy a 🥖 loaf for \$5
+- but a 🍰 Pastery for \$2
+
+Manager special!:
+Bread is buy 2 get one free!
+Pastery of your choice 3 for \$5!
+
+What would you like?  
+Bread: (int input)
+Pastery:(int input )
+
+---
+
+Restate order:
+your total is: (int)
+
+## Specs
+
+```
+give cost with no deal
+Bread input: 3
+Pastery input: 4
+output: 3 breads ordered at $5 each and 4 pasteries orderd $2 each
+(3*5)= $15 for bread  & (4*)2= $8 for pastery
+```
+
+```
+Bread Deal check
+Bread input: 3
+Pastery input: 4
+output: for every 3 bread its 5$ off, 3 breads and its $10 (3 for $10)
+```
+
+```
+pastery deal check
+Bread input: 3
+Pastery input: 4
+output: for every 3 pasteries its $1 dollar off (3 for $5)
+```
+
+## Technologies Used
+
+C#
+
+### License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Copyright (c) 2020 **_Natalie Murphy_**
